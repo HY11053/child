e9481958b68dfe2968337ca725257d6e51b5ac5b
liang <liang569874@163.com>
2017-03-11 17:40:24 +0800

    v_6

--- resources/views/emails/phonemailsend.blade.php
+++ resources/views/emails/phonemailsend.blade.php
@@ -2,7 +2,7 @@
     <li  class="list-group-item active"> 电话提交信息</li>
     <li  class="list-group-item">姓名： {{$name}}</li>
     <li  class="list-group-item"> 电话：{{$phoneno}}</li>
-    <li  class="list-group-item"> 微信号：{{$wechat}}</li>
+    <li  class="list-group-item"> 微信号：{{$weichat}}</li>
     <li  class="list-group-item"> 宝宝年龄：{{$age}}</li>
     <li  class="list-group-item"> 地址：{{$address}}</li>
     <li  class="list-group-item"> IP：{{$ip}}</li>
